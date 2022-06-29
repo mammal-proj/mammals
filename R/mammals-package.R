a0626b27d271bf95e29b5811c5dab00e84a237b2
Matías Castillo <matcasti@umag.cl>
2022-06-29 15:09:23 -0300

    add package level documentation

---
+++ R/mammals-package.R
@@ -0,0 +1,10 @@
+#' @title Population Dynamics Analysis
+#'
+#' @description Tools for the implementation of population dynamics analysis.
+#'
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
