759b00fd1ab60287fd057c95a4ffd46448f5b586
Matías Castillo <matcasti@umag.cl>
2022-07-10 18:48:12 -0300

    datos y docs #2
    - importación de datos y una mínima plantilla para añadir la documentacion

--- cran-comments.md
+++ cran-comments.md
@@ -2,4 +2,4 @@
 
 0 errors | 0 warnings | 0 note
 
-* This is a new release. Esto es posible gracias al trabajo de Carla, José y Renato.
+* This is a new release.
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,24 @@
-## code to prepare `DATASET` dataset goes here
+## code to prepare `sealions` dataset
 
-usethis::use_data(DATASET, overwrite = TRUE)
+
+# Load libraries ------------------------------------------------------------------------------
+
+  library(data.table)
+
+# Load dataset --------------------------------------------------------------------------------
+
+  sealions <- data.table::fread("data-raw/historicos.csv", na.strings = c("", "nan", "NA"))
+
+# Further processing --------------------------------------------------------------------------
+
+  # Non-numeric columns
+  ind <- sealions[, names(.SD), .SDcols = !is.numeric]
+  lapply(
+    X = sealions[, ..ind],
+    FUN = unique
+  )
+  sealions[, (ind) := lapply(.SD, as.factor), .SDcols = ind]
+
+# Exporting the dataset -----------------------------------------------------------------------
+
+  usethis::use_data(sealions, overwrite = TRUE)
