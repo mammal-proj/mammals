c7b4e45a090630d00af38263129c49fc64210331
Matías Castillo <matcasti@umag.cl>
2022-07-27 08:54:59 -0300

    dataset already transfered to jffs pacakge

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -7,18 +7,8 @@
 
 # Load dataset --------------------------------------------------------------------------------
 
-  sealions <- data.table::fread("data-raw/historicos.csv", na.strings = c("", "nan", "NA"))
-
 # Further processing --------------------------------------------------------------------------
-
-  # Non-numeric columns
-  ind <- sealions[, names(.SD), .SDcols = !is.numeric]
-  lapply(
-    X = sealions[, ..ind],
-    FUN = unique
-  )
-  sealions[, (ind) := lapply(.SD, as.factor), .SDcols = ind]
 
 # Exporting the dataset -----------------------------------------------------------------------
 
-  usethis::use_data(sealions, overwrite = TRUE)
+  usethis::use_data(NULL, overwrite = TRUE)
